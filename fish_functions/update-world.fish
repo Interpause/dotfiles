3fb59d877d6d8f4dbdd8b33b627f4e3359e86453
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-05-16 19:14:12 +0800

    update some util scripts

--- fish_functions/update-world.fish
+++ fish_functions/update-world.fish
@@ -1,8 +1,13 @@
 function update-world --description 'Updates all packages then cleans'
+  #export FEATURES="-distcc"
   sudo emerge -NDu --with-bdeps=y @world
-  #sudo emerge -c
-  #sudo emerge @preserved-rebuild
-  #sudo lto-rebuild -r
-  #sudo revdep-rebuild -v
+  #sudo emerge @golang-rebuild
+  #sudo emerge @rust-rebuild
+  #sudo emerge @module-rebuild
   #sudo perl-cleaner --all
+  #perl-cleaner --reallyall -- -av
+  #sudo haskell-updater
+  #sudo lto-rebuild -r
+  #sudo revdep-rebuild -vi
+  #sudo emerge -c
 end
