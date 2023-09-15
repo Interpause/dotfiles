fe61e6913e361d1f445b00e034d9513541ae367f
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-09-15 20:40:07 +0800

    improve open-lsw command
    - discard virsh error code
    - independent launch of looking glass client
    - sleep to wait for vm service to be ready

--- fish_functions/open-lsw.fish
+++ fish_functions/open-lsw.fish
@@ -1,7 +1,8 @@
 function open-lsw --description 'Open Linux Subsystem for Windows'
   if not rc-service -q libvirtd status
     up-vm
+    sleep 1
   end
-  virsh --connect qemu:///system start win11
-  looking-glass-client
+  virsh --connect qemu:///system start win11 || true
+  nohup looking-glass-client &
 end
