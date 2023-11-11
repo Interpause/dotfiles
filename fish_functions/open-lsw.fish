44476b5d2db730246a7514a2370fe6fcea861a09
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-11-11 18:58:18 +0800

    tweak open-lsw a bit

--- fish_functions/open-lsw.fish
+++ fish_functions/open-lsw.fish
@@ -1,7 +1,7 @@
 function open-lsw --description 'Open Linux Subsystem for Windows'
   if not rc-service -q libvirtd status
     up-vm
-    sleep 1
+    sleep 0.1
   end
   virsh --connect qemu:///system start win11 || true
   nohup looking-glass-client >/dev/null 2>&1 &
