7d0ccaba0b581937a987549306ec14f31cb8580c
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-02-12 12:03:56 +0800

    improve dedupe function

--- fish_functions/dedupe-btrfs.fish
+++ fish_functions/dedupe-btrfs.fish
@@ -1,3 +1,4 @@
 function dedupe-btrfs --description 'Use duperemove to dedupe'
-  sudo duperemove -rqd --hashfile=/backup/duperemove.hash --exclude="/var/lib/docker/btrfs*" / /home
+  sudo duperemove -rqd --hashfile=/backup/duperemove-root.hash --dedupe-options=partial --exclude="/var/lib/docker/btrfs*" /
+  sudo duperemove -rqd --hashfile=/backup/duperemove-home.hash --dedupe-options=partial /home
 end
