becf8807a44253d37d9a95bbfd84d11b52aa1407
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-08-03 02:33:16 +0800

    add support for kvmfr when using looking glass

--- fish_functions/open-lsw.fish
+++ fish_functions/open-lsw.fish
@@ -3,6 +3,14 @@ function open-lsw --description 'Open Linux Subsystem for Windows'
     up-vm
     sleep 0.1
   end
+  # https://looking-glass.io/docs/B7-rc1/ivshmem_kvmfr
+  if not lsmod | grep -q kvmfr
+    sudo rm -f /dev/kvmfr0
+    sudo modprobe kvmfr
+  set user (whoami)
+	sudo chown $user:$user /dev/kvmfr0
+  end
   virsh --connect qemu:///system start win11 || true
-  looking-glass-client >/dev/null 2>&1 &; disown
+  # Use XWayland for sharpness till Wayland fractional scaling is fixed
+  env -u WAYLAND_DISPLAY looking-glass-client >/dev/null 2>&1 &; disown
 end
