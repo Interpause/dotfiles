e0c1e4f9fcb252257ddd18154e2c8885c1acc984
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-08-29 22:55:44 +0800

    feat(fish): config fzf alt-c command

--- cfg2.fish
+++ cfg2.fish
@@ -10,6 +10,7 @@ set -g fish_handle_reflow 1
 if command -q fd
   and command -q fzf
   set -gx FZF_CTRL_T_COMMAND "fd --type f -u --strip-cwd-prefix --color=always"
+  set -gx FZF_ALT_C_COMMAND "fd --type d -u --strip-cwd-prefix --color=always"
   set -gx FZF_DEFAULT_COMMAND "fd --type f -u --strip-cwd-prefix --color=always"
   set -gx FZF_DEFAULT_OPTS "--ansi --tmux"
   fzf --fish | source
