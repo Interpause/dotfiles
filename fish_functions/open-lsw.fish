f6d9ec323e3888b4ab48d92f70db9a1f4f505f8e
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-08-29 22:57:04 +0800

    fix(fish): open-lsw: disable kvmfr as it breaks virtiofsd

--- fish_functions/open-lsw.fish
+++ fish_functions/open-lsw.fish
@@ -4,12 +4,13 @@ function open-lsw --description 'Open Linux Subsystem for Windows'
     sleep 0.1
   end
   # https://looking-glass.io/docs/B7-rc1/ivshmem_kvmfr
-  if not lsmod | grep -q kvmfr
-    sudo rm -f /dev/kvmfr0
-    sudo modprobe kvmfr
-  set user (whoami)
-	sudo chown $user:$user /dev/kvmfr0
-  end
+  # Breaks virtiofs, see: https://gitlab.com/qemu-project/qemu/-/issues/1877
+  # if not lsmod | grep -q kvmfr
+  #   sudo rm -f /dev/kvmfr0
+  #   sudo modprobe kvmfr
+  #   set user (whoami)
+  #   sudo chown $user:$user /dev/kvmfr0
+  # end
   virsh --connect qemu:///system start win11 || true
   # Use XWayland for sharpness till Wayland fractional scaling is fixed
   env -u WAYLAND_DISPLAY looking-glass-client >/dev/null 2>&1 &; disown
